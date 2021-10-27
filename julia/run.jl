5f81cc78979a281a75eb21d9f3a47cbb7b18ec87
Evgeny Metelkin <metelkin@insysbio.com>
2021-10-27 17:51:35 +0300

    add dataset

--- julia/run.jl
+++ julia/run.jl
@@ -5,6 +5,7 @@ p = load_platform(".")
 scenarios = read_scenarios("julia/scenarios.csv")
 add_scenarios!(p, scenarios)
 
-
+data = read_measurements("julia/data.csv")
+add_measurements!(p, data)
 
 sim(p) |> plot
