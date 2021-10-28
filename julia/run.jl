ff5f7907eae43c052e04d1ea92f6949596941c8a
Evgeny Metelkin <metelkin@insysbio.com>
2021-10-28 16:13:39 +0300

    use abs(C)

--- julia/run.jl
+++ julia/run.jl
@@ -28,7 +28,7 @@ fig = sim(p, scenarios=[:scn2], parameters_upd=optim(res_fit_2)) |> plot
 # savefig(fig, "diagnostics/scn2_best.png")
 
 
-# fitting 3 best -126.25
+# fitting 3 best -126.25 / -95.83
 params_df_3 = read_parameters("./julia/parameters-3.csv")
 res_fit_3 = fit(p, params_df_3; scenarios=[:scn3])
 res_fit_3 = fit(p, optim(res_fit_3); scenarios=[:scn3])
