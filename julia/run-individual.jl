efa81360ad73eff40d17d5b3cff8730e03c4a14d
Evgeny Metelkin <metelkin@insysbio.com>
2021-11-16 16:48:26 +0300

    mixed sigma

--- julia/run-individual.jl
+++ julia/run-individual.jl
@@ -12,9 +12,9 @@ add_measurements!(p, data)
 sim(p) |> plot
 sim(p, parameters_upd = [:Vmax=>0.3, :ke=>0., :k_a=>5.]) |> plot
 
-# fitting 1 best -49.85/-69.17, sigma_add = 2.23/0.039
+# fitting 1 best -73.48/-80.87, sigma_add = 0.035/ sigma_prop=0.081
 params_df_1 = read_parameters("./julia/parameters-1.csv")
-res_fit_1 = fit(p, params_df_1; scenarios=[:scn1])
-res_fit_1 = fit(p, optim(res_fit_1); scenarios=[:scn1])
+res_fit_1 = fit(p, params_df_1; scenarios=[:scn1], ftol_abs=1e-6, ftol_rel=0.)
+res_fit_1 = fit(p, optim(res_fit_1); scenarios=[:scn1], ftol_abs=1e-6, ftol_rel=0.)
 fig = sim(p, scenarios=[:scn1], parameters_upd=optim(res_fit_1)) |> plot
 # savefig(fig, "diagnostics/scn1_best.png")
--- julia/run.jl
+++ julia/run.jl
@@ -2,10 +2,10 @@ using HetaSimulator, Plots
 
 p = load_platform(".")
 
-scenarios = read_scenarios("julia/scenarios.csv")
+scenarios = read_scenarios("data-mumenthaler-2000/scenarios.csv")
 add_scenarios!(p, scenarios)
 
-data = read_measurements("julia/data.csv")
+data = read_measurements("data-mumenthaler-2000/data.csv")
 add_measurements!(p, data)
 
 # initial plot
@@ -28,7 +28,7 @@ fig = sim(p, scenarios=[:scn2], parameters_upd=optim(res_fit_2)) |> plot
 # savefig(fig, "diagnostics/scn2_best.png")
 
 
-# fitting 3 best -126.25 / -95.83
+# fitting 3 best -102.97
 params_df_3 = read_parameters("./julia/parameters-3.csv")
 res_fit_3 = fit(p, params_df_3; scenarios=[:scn3])
 res_fit_3 = fit(p, optim(res_fit_3); scenarios=[:scn3])
